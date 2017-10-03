5fd60111341d895a1cd996a9b0ad398b86c734a9
JuLiobobo <julien.bonnardel94@gmail.com>
2017-10-03 22:53:33 +0200

    Overriding Form login ok

--- src/BetterknowBundle/Controller/DefaultController.php
+++ src/BetterknowBundle/Controller/DefaultController.php
@@ -4,14 +4,16 @@ namespace BetterknowBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
+     * @Security("has_role('ROLE_ADMIN')")
      */
     public function indexAction()
     {
-        return $this->render('BetterknowBundle:Default:index.html.twig');
+        return $this->render('BetterknowBundle:Security:login.html.twig');
     }
 }
