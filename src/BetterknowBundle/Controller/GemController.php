ff16754e2a1a7135348b1ab40ccbed860aa9fa86
JuLiobobo <julien.bonnardel94@gmail.com>
2017-10-24 20:23:34 +0200

    next api

--- src/BetterknowBundle/Controller/GemController.php
+++ src/BetterknowBundle/Controller/GemController.php
@@ -7,11 +7,19 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\ViewHandler;
+use FOS\RestBundle\View\View;
 use BetterknowBundle\Entity\Gem;
 
 class GemController extends Controller
 {
-
+    
+    /**
+     * @Rest\View()
+     * @Rest\Get("/gems")
+     */
     public function getGemsAction(Request $request)
     {
         
@@ -20,7 +28,8 @@ class GemController extends Controller
                 ->findAll();
         
         /* @var $gems Gem[] */
-
+        
+        
         $formatted = [];
         foreach ($gems as $gem) {
             $formatted[] = [
@@ -29,26 +38,41 @@ class GemController extends Controller
                'timeReceive' => $gem->getTimeReceive(),
             ];
         }
+        
+        // Création d'une vue FOSRestBundle
+        $view = View::create($formatted);
+        $view->setFormat('json');
 
-        return new JsonResponse($formatted);
+        return $this->get('fos_rest.view_handler')->handle($view);
+        
     }
     
-
-    public function getGemAction($id, Request $request)
+    /**
+     * @Rest\View()
+     * @Rest\Get("/gems/{gem_id}")
+     */
+    public function getGemAction(Request $request)
     {
         $gem = $this->get('doctrine.orm.entity_manager')
                 ->getRepository('BetterknowBundle:Gem')
-                ->find($id);
+                ->find($request->get('gem_id'));
         
         /* @var $gem Gem */
         
+        if (empty($gem)) {
+            return new JsonResponse(['message' => 'Gem not found'], Response::HTTP_NOT_FOUND);
+        }
         
         $formatted = [
            'id' => $gem->getId(),
             'gender' => $gem->getGender(),
             'timeReceive' => $gem->getTimeReceive(),
         ];
+        
+        // Création d'une vue FOSRestBundle
+        $view = View::create($formatted);
+        $view->setFormat('json');
 
-        return new JsonResponse($formatted);
+        return $this->get('fos_rest.view_handler')->handle($view);
     }
 }
