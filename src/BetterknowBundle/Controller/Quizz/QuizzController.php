496fc6eb688250a2aa43c7d33e01e13fb609f541
juliobobo <julien.bonnardel94@gmail.com>
2017-11-04 17:32:58 +0000

    quizz choice done

--- src/BetterknowBundle/Controller/Quizz/QuizzController.php
+++ src/BetterknowBundle/Controller/Quizz/QuizzController.php
@@ -11,9 +11,12 @@ use FOS\RestBundle\View\ViewHandler;
 use FOS\RestBundle\View\View;
 use BetterknowBundle\Entity\Quizz;
 use BetterknowBundle\Form\QuizzType;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class QuizzController extends Controller
 {
+    
+    const MIN_FRIEND = 5;
     
     /**
      * @Rest\View(serializerGroups={"quizz"})
@@ -92,6 +95,92 @@ class QuizzController extends Controller
         $this->em()->flush();
         
         return $this->succes();
+    }
+    
+    
+    /**
+     * @Rest\View(statusCode=Response::HTTP_CREATED, serializerGroups={"response"})
+     * @Rest\Get("/responses/{id_user}")
+     */
+    public function getChoiceResponsesAction(Request $request)
+    {
+        $user = $this->em()->getRepository('BetterknowBundle:User')
+                ->find($request->get('id_user'));
+        
+        $friendList = $user->getFriends();
+        
+        $choice_list = new ArrayCollection();
+        $count = 0;
+        
+        if(count($friendList) < self::MIN_FRIEND) {
+            // On va chercher les amis de mes amis recursivité
+           return $this->searchFriends($friendList);
+        } else {
+            foreach($friendList as $f){
+                if(rand(0, 1) == 1){
+                    $choice_list->add($f->getFriend());
+                    $count++;
+                }
+                
+                if($count == 4) break;
+            }
+        
+        }
+    }
+    
+    /**
+     * @Rest\View(statusCode=Response::HTTP_CREATED, serializerGroups={"quizz"})
+     * @Rest\Post("/quizz/{id_quizz}/response/{id_response}")
+     */
+    public function postResponseAction(Request $request)
+    {
+        $quizz = $this->em()->getRepository('BetterknowBundle:Quizz')
+                    ->find($request->get('id_quizz'));
+        
+        if (empty($quizz)) {
+            return $this->notFound('Quizz');
+        }
+        
+        $response = $this->em()->getRepository('BetterknowBundle:User')
+                    ->find($request->get('id_response'));
+        
+        if (empty($response)) {
+            return $this->notFound('User');
+        }
+        
+        $quizz->addResponse($response);
+        
+        $this->em()->flush();
+        
+        return $quizz;
+    }
+    
+    
+    private function searchFriends($friendList){
+        
+        $list = new ArrayCollection();
+        
+        foreach($friendList as $f){
+            $myFriend = $this->em()->getRepository('BetterknowBundle:User')
+                    ->find($f->getFriend()->getId());
+          
+            if(count($myFriend->getFriends()) > self::MIN_FRIEND){
+                $count = 0;
+                foreach($myFriend->getFriends() as $m){
+                    if(rand(0,1) == 1){
+                        $list->add($m->getFriend());
+                        $count++;
+                        
+                        if($count == 4) break;
+                    }            
+                }
+                return $list;
+            }
+        }
+        
+        if(count($list) == 0){
+            $this->searchFriends($myFriend->getFriends());
+        }
     }
     
     private function em(){
